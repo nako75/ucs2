2a2bcefc77ab48c4720304d47f6fdf3d3fbde664
なこ <73942801+nako75@users.noreply.github.com>
2026-07-22 02:33:26 +0800

    uhhh

--- Ultrapowa Clash Server/PacketProcessing/Messages/Server/AvatarProfileMessage.cs
+++ Ultrapowa Clash Server/PacketProcessing/Messages/Server/AvatarProfileMessage.cs
@@ -9,7 +9,6 @@ using UCS.Core;
 
 namespace UCS.PacketProcessing
 {
-    //Packet 24334
     class AvatarProfileMessage : Message
     {
         private Level m_vLevel;
@@ -30,17 +29,6 @@ namespace UCS.PacketProcessing
             pack.AddInt32(ch.GetHomeJSON().Length + 4);
             pack.AddInt32(unchecked((int)0xFFFF0000));
             pack.AddRange(ch.GetHomeJSON());
-
-            // --- BAGIAN YANG GW PERBAIKI BREE ---
-            ClientAvatar avatar = (ClientAvatar)m_vLevel.GetPlayerAvatar();
-            
-            // Mengganti 13 byte nol bawaan Ultrapowa dengan struktur data profil yang benar
-            pack.AddInt32(0); // 1. Troops Donated 
-            pack.AddInt32(0); // 2. Troops Received
-            pack.AddInt32(0); // 3. War Stars Won
-            pack.AddInt32(avatar.GetAllTimeBestScore()); // 4. <-- INI ALL TIME BEST-NYA MASUK SINI!
-            pack.Add(0);      // 5. 1 Byte flag/padding terakhir biar klien nggak crash
-            // ------------------------------------
 
             SetData(pack.ToArray());
         }
