bf6df070c59f9ca8fd4e111d7e7024f068d96aa6
なこ <73942801+nako75@users.noreply.github.com>
2026-07-23 02:39:28 +0800

    updste little

--- Ultrapowa Clash Server/PacketProcessing/Messages/Client/StartWarSearchMessage.cs
+++ Ultrapowa Clash Server/PacketProcessing/Messages/Client/StartWarSearchMessage.cs
@@ -8,13 +8,13 @@ namespace UCS.PacketProcessing.Messages.Client
     // ID Paket: 14331 (Dieksekusi pas Leader/Co-Leader klik tombol Start War)
     internal class StartWarSearchMessage : Message
     {
-        public StartWarSearchMessage(Client client, BinaryReader br) : base(client, br)
+        // FIX CS0118: Pakai "UCS.PacketProcessing.Client" supaya gak bentrok sama nama namespace
+        public StartWarSearchMessage(UCS.PacketProcessing.Client client, BinaryReader br) : base(client, br)
         {
         }
 
         public override void Decode()
         {
-            // Client ngirim ID clan dan parameter search, kita tangkap dengan try-catch agar aman
             try { } catch (Exception) { }
         }
 
@@ -28,7 +28,7 @@ namespace UCS.PacketProcessing.Messages.Client
                 Alliance alliance = ObjectManager.GetAlliance(allianceId);
                 if (alliance != null)
                 {
-                    // Aktifkan status war dan set durasi 1 Jam (3600 detik dari waktu sekarang)
+                    // Aktifkan status war dan set durasi 1 Jam (3600 detik)
                     int currentEpoch = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
                     alliance.IsInWar = true;
                     alliance.WarEndTime = currentEpoch + 3600; // +1 Jam
